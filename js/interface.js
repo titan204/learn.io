b47ef60a2a5d82ce2b5cc791197d22c74c5c207c
titan <mohamedyasser22992299@gmail.com>
2025-09-10 21:46:56 +0300

    :)))

---
+++ js/interface.js
@@ -0,0 +1,12 @@
+const box = document.querySelector('.gradient-transition-box');
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        box.classList.add('transitioned');
+    }, 4000);
+});
+
+
+setTimeout(()=>{
+    window.location="html/login.html";
+},7000)
